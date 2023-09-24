ebfc565c97a3b6345eac8e8357b6b9841207252b
Abrar <hossainabrar@aol.com>
2023-09-23 21:39:49 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "abrar",
+    streetAddress: "broadway",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newEmployee = {...obj};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj;
+};
+function deleteFromEmployeeByKey(obj, key){
+    const deleteEmployee = {...obj};
+    delete deleteEmployee[key];
+    return deleteEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+};
